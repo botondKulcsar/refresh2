8772265b0dd2d0bcd1d571bc06baffd9590285e9
Botond Kulcsar <bkulcsar@gmx.com>
2021-02-08 00:41:20 +0200

    added a diary app

---
+++ ex/diary/index.js
@@ -0,0 +1,32 @@
+const entryForm = document.getElementById('entryForm');
+const entrySection = document.querySelector('#entries');
+const entryTextbox = document.querySelector('.entry-textbox');
+const entriesNav = document.querySelector('.entries-nav');
+
+let count = 1;
+
+
+
+function addEntryToDom(event) {
+    event.preventDefault();
+    const entryDiv = document.createElement('div');
+    entryDiv.className = 'single-entry';
+    entryDiv.innerText = entryTextbox.value;
+    entryDiv.style.display = 'none';
+    entrySection.appendChild(entryDiv);
+    entryForm.reset();
+    // entryTextbox.value = '';
+    const displayEntryButton = document.createElement('button');
+    displayEntryButton.className = 'display-entry-button';
+    displayEntryButton.innerText = count;
+
+    displayEntryButton.addEventListener('click', () => {
+        entryDiv.style.display = 'block';
+    })
+
+    entriesNav.appendChild(displayEntryButton);
+
+    count++;
+}
+
+entryForm.addEventListener('submit', addEntryToDom);
