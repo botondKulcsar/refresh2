a29da14f3cb1619acf63a33eaf60cee170b25b86
Botond Kulcsar <bkulcsar@gmx.com>
2021-02-12 16:04:57 +0200

    dice game is FAIR

--- ex/dice-game/index.js
+++ ex/dice-game/index.js
@@ -1,5 +1,7 @@
 // Create variables for the game state
 let player1Score = 0;
+let player1Rolls = 0;
+let player2Rolls = 0;
 let player2Score = 0;
 let player1Turn = true;
 
@@ -14,8 +16,7 @@ const player2Dice = document.getElementById('player2Dice');
 const rollBtn = document.getElementById('rollBtn');
 const resetBtn = document.getElementById('resetBtn');
 
-function gameOver(player) {
-    message.textContent = 'Player ' + player + ' has won!';
+function gameOver() {
     rollBtn.style.display = 'none';
     resetBtn.style.display = 'inline';
 }
@@ -24,6 +25,7 @@ rollBtn.addEventListener('click', () => {
     const randomNumber = Math.floor(Math.random() * 6) + 1;
 
     if (player1Turn) {
+        player1Rolls++;
         player1Score += randomNumber;
         player1Scoreboard.textContent = player1Score;
         player1Dice.textContent = randomNumber;
@@ -32,6 +34,7 @@ rollBtn.addEventListener('click', () => {
         player2Dice.classList.add('active');
         message.textContent = 'Player 2 Turn';
     } else {
+        player2Rolls++;
         player2Score += randomNumber;
         player2Scoreboard.textContent = player2Score;
         player2Dice.textContent = randomNumber;
@@ -41,10 +44,12 @@ rollBtn.addEventListener('click', () => {
         message.textContent = 'Player 1 Turn';
     }
 
-    if (player1Score >= 20) {
-        gameOver(1);
-    } else if (player2Score >= 20) {
-        gameOver(2);
+    if (player1Score >= 20 && !(player2Rolls < player1Rolls) && (player1Score > player2Score)) {
+        message.textContent = 'Player 1 has won!';
+        gameOver();
+    } else if (player2Score >= 20 && (player2Score > player1Score)) {
+        message.textContent = 'Player 2 has won!';
+        gameOver();
     }
 
     player1Turn = !player1Turn;
