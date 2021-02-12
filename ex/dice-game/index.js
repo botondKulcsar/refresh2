344b94bb80c64a1d4e92c856f5f4988bcceb9ac6
Botond Kulcsar <bkulcsar@gmx.com>
2021-02-12 16:42:36 +0200

    added Double or Nothing function

--- ex/dice-game/index.js
+++ ex/dice-game/index.js
@@ -5,6 +5,8 @@ let player2Rolls = 0;
 let player2Score = 0;
 let player1Turn = true;
 
+let randomNumber;
+
 const message = document.getElementById('message');
 
 const player1Scoreboard = document.getElementById('player1Scoreboard');
@@ -15,6 +17,7 @@ const player2Dice = document.getElementById('player2Dice');
 
 const rollBtn = document.getElementById('rollBtn');
 const resetBtn = document.getElementById('resetBtn');
+const doubleBtn = document.getElementById('doubleOrNothing');
 
 function gameOver() {
     rollBtn.style.display = 'none';
@@ -22,28 +25,52 @@ function gameOver() {
 }
 
 rollBtn.addEventListener('click', () => {
-    const randomNumber = Math.floor(Math.random() * 6) + 1;
+    randomNumber = Math.floor(Math.random() * 6) + 1;
 
     if (player1Turn) {
-        player1Rolls++;
-        player1Score += randomNumber;
-        player1Scoreboard.textContent = player1Score;
-        player1Dice.textContent = randomNumber;
-
-        player1Dice.classList.remove('active');
-        player2Dice.classList.add('active');
-        message.textContent = 'Player 2 Turn';
+        rollDicePlayer1();
     } else {
-        player2Rolls++;
-        player2Score += randomNumber;
-        player2Scoreboard.textContent = player2Score;
-        player2Dice.textContent = randomNumber;
-
-        player2Dice.classList.remove('active');
-        player1Dice.classList.add('active');
-        message.textContent = 'Player 1 Turn';
+        rollDicePlayer2();
     }
 
+    checkIfOver();
+
+    player1Turn = !player1Turn;
+})
+
+function rollDicePlayer1(double = false, nothing = false) {
+    player1Rolls++;
+    checkIfDoubleOrNothing(double, nothing);
+    player1Score += randomNumber;
+    player1Scoreboard.textContent = player1Score;
+    player1Dice.textContent = randomNumber;
+
+    player1Dice.classList.remove('active');
+    player2Dice.classList.add('active');
+    message.textContent = 'Player 2 Turn';
+}
+
+function rollDicePlayer2(double = false, nothing = false) {
+    player2Rolls++;
+    checkIfDoubleOrNothing(double, nothing);
+    player2Score += randomNumber;
+    player2Scoreboard.textContent = player2Score;
+    player2Dice.textContent = randomNumber;
+
+    player2Dice.classList.remove('active');
+    player1Dice.classList.add('active');
+    message.textContent = 'Player 1 Turn';
+}
+
+function checkIfDoubleOrNothing(double, nothing) {
+    if (double && !nothing) {
+        randomNumber *= 2;
+    } else if (!double && nothing) {
+        randomNumber = 0;
+    }
+}
+
+function checkIfOver() {
     if (player1Score >= 20 && !(player2Rolls < player1Rolls) && (player1Score > player2Score)) {
         message.textContent = 'Player 1 has won!';
         gameOver();
@@ -51,6 +78,26 @@ rollBtn.addEventListener('click', () => {
         message.textContent = 'Player 2 has won!';
         gameOver();
     }
+}
+
+doubleBtn.addEventListener('click', () => {
+    randomNumber = Math.floor(Math.random() * 6) + 1;
+    let double = true;
+    let nothing = true;
+    const random = Math.random();
+    if (random < 0.5) {
+        double = false;
+    } else {
+        nothing = false;
+    }
+
+    if (player1Turn) {
+        rollDicePlayer1(double, nothing);
+    } else {
+        rollDicePlayer2(double, nothing);
+    }
+
+    checkIfOver();
 
     player1Turn = !player1Turn;
 })
